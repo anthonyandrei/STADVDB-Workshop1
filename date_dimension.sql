3b9a1045008ba3275999685a816990ff7b5e9085
Ramon John L. Dela Cruz <119769062+MonZenn@users.noreply.github.com>
2025-09-27 09:30:40 +0800

    Create date_dimension.sql

---
+++ date_dimension.sql
@@ -0,0 +1,30 @@
+CREATE TABLE date_dimension (
+    tripID INT PRIMARY KEY,
+    pickup_year INT,
+    pickup_month INT,
+    pickup_day INT,
+    dropoff_year INT,
+    dropoff_month INT,
+    dropoff_day INT,
+    trip_hours DECIMAL(6,2)
+);
+INSERT INTO date_dimension
+SELECT
+    tripID,
+    YEAR(STR_TO_DATE(tpep_pickup_datetime, '%c/%e/%Y %l:%i %p'))  AS pickup_year,
+    MONTH(STR_TO_DATE(tpep_pickup_datetime, '%c/%e/%Y %l:%i %p')) AS pickup_month,
+    DAY(STR_TO_DATE(tpep_pickup_datetime, '%c/%e/%Y %l:%i %p'))   AS pickup_day,
+    YEAR(STR_TO_DATE(tpep_dropoff_datetime, '%c/%e/%Y %l:%i %p'))  AS dropoff_year,
+    MONTH(STR_TO_DATE(tpep_dropoff_datetime, '%c/%e/%Y %l:%i %p')) AS dropoff_month,
+    DAY(STR_TO_DATE(tpep_dropoff_datetime, '%c/%e/%Y %l:%i %p'))   AS dropoff_day,
+    ROUND(
+        TIMESTAMPDIFF(MINUTE,
+            STR_TO_DATE(tpep_pickup_datetime, '%c/%e/%Y %l:%i %p'),
+            STR_TO_DATE(tpep_dropoff_datetime, '%c/%e/%Y %l:%i %p')
+        ) / 60, 2
+    ) AS trip_hours
+-- NOTE: This is in percentage to easy calculate number of hours :>
+FROM denormalized_taxi;
+
+SELECT * 
+FROM date_dimension;
